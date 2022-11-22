b800a0093c46873d94ef2bb51f1752b0d8f1c5f0
jharambha <97498357+jharambha@users.noreply.github.com>
2022-11-22 18:50:50 +0530

    hiuoghiug

--- src/pages/write/write.jsx
+++ src/pages/write/write.jsx
@@ -2,6 +2,7 @@ import "./write.css";
 
 export default function Write(){
 return(
+    
      <div className="write">
         <form  className="writeForm">
             <div className="writeFormGroup">
